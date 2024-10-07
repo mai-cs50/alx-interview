153aa8c502567c1ffe6608d2ec1a973f7ce885ba
mai-cs50 <maimohamed.1992@yahoo.com>
2024-10-07 11:40:21 -0700

    s

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -17,8 +17,8 @@ def pascal_triangle(n):
     for i in range(1, n):  # Loop to generate n rows
         row = [1]  # Start each row with the first element as 1
 
-        for j in range(len(triangle[i - 1]) - 1):  
-	    # Calculate the inner elements
+        for j in range(len(triangle[i - 1]) - 1):
+            # Calculate the inner elements
             row.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
 
         row.append(1)  # Append the last element as 1
